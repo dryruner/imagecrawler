25a91483df5673b1f101c6319b4a9528849489e4
jonnyhsy <jonnyhsy@foxmail.com>
2016-10-16 06:17:46 -0700

    Update image_crawler v1.04. [New features]: 1. wget and curl support. 2. Multi-process background job support, performance is enhanced by 10x. 3. Google/Baidu image search url format update, now the script works again.
    Signed-off-by: jonnyhsy <jonnyhsy@foxmail.com>

--- README.md
+++ README.md
@@ -1,21 +1,40 @@
-# image\_crawler v1.0.3
+# image\_crawler v1.0.4
 This is an image crawler in pure shell script, which could download the images (actually the image URLs on the web) once given a keyword. It fakes to visit Google Image/Baidu Image, using the keywords you provide to perform like a human search, and then parse and record the results(image urls) returned by Google or Baidu. Once you have the image urls, you could download the real images using any script languages you like. This tool could also be used to compare the search quality and relevance between Google Image and Baidu Image.
+
+## What's NEW?
+* Now it supports both wget and curl!
+* Performance is enhanced ~10X faster, increasing the parallelism by utilizing multi-process background jobs in bash.
+* Baidu/Google image search urls are updated. This script was created ~5 years ago, and things have changed a lot since then, both baidu and google changed their image search query parameters and rules. So I took quite a few time this weekend to figure out what's the change, and update the script to make it work again.
+* A python script is introduced to decode the baidu image objURL. I planned to do it in bash script originally, but you need to introduce a hashtable, damn complicated to make it work in bash only... So I'll leave it as a TODO now - to update this python script to the bash script with the same functionality.
+
+## TODO:
+* Continue to improve the performance.
+* Adding proxy support? 
+* Implement the python equivalent to decode the baidu image url.
+* ...
 
 ## How to use it?
 * Input: A file named query\_list.txt, per keyword per line.
 * Usage: 
-	$./image\_crawler.sh google num;
+	$./image\_crawler.sh google <num>;
 	* google could be replaced by baidu
-	* num is the number of images you want to download for a given keyword.
-* Output: The script will generate a directory named google/(or baidu/ as you choosed), which contains files of the format: "i\_objURL-list\_keyword[i]", i is the ith keyword in the query\_list.txt. In each of these files contain num lines, per image url per line.
+	* <num> is the number of images you want to download for a given keyword.
+* Output: The script generates a directory named google/(or baidu/ as you choosed), which contains files of the format: "i\_objURL-list\_keyword[i]", i is the ith keyword in the query\_list.txt. In each of these files contain num lines, per image url per line.
 
 ## Performance
 I've tested this script with 10 keywords (just as in the query\_list.txt), each keyword crawling 300 results using Google.<br/> 
 Results are as follows:<br/>
-     [unix14 ~/imagecrawler]$ time ./image\_crawler.sh google 300 <br/>
-     0.396u 1.298s 0:31.91 5.2%	0+0k 0+23472io 0pf+0w <br/>
-32s for 3000 results, and in the future I'll tweak it into a more concurrent version. <br/>
+[unix14 ~/imagecrawler]$ time ./image\_crawler.sh google 300 <br/>
+real	0m5.766s
+user	0m2.425s
+sys	0m2.254s
+
+[unix14 ~/imagecrawler]$ time ./image\_crawler.sh baidu 300 <br/>
+real	0m11.419s
+user	0m1.254s
+sys	0m1.044s
+
+The result is not bad, and in the future I'll tweak it into a more concurrent version. <br/>
 
 # Note:
-It works perfectly in linux platform(Ubuntu, Red Hat, etc.).
-But it complains fault in Mac OS X terminal when crawling images from google(crawling from baidu is OK), because the sed utility is somewhat different in Mac and Linux platforms. Maybe I'll look into and fix it in the future.
+It works in any platform that supports bash, egrep, awk, python, wget | curl. So, Ubuntu, MacOS, etc.
--- image_crawler.sh
+++ image_crawler.sh
@@ -3,19 +3,56 @@
 # is the source of the images you wanna crawl. You could try twice and get the 
 # objURLs from baidu and google respectively. 
 
+# Debug options
+#set -o xtrace
+
+###################### Config BEGIN ################################
+
 from=$1;
 num=$2;
+CRAWLER=
+OUTPUT_FLAG=-O     # For curl using '-o' instead.
+RETRY_FLAG=-t      # Default retrying flag (for wget). For curl using '--retry <num>'.
+RETRY_NUM=3        # Crawler retries <num> times if here's a connection issue.
+QUITE_FLAG=-q      # Turning off crawler's logging output. For curl using '-s'.
+FOLLOW_REDIRECT=   # Following the redirection, in case 301 moved was returned. For curl using '-L'.
+ROBOTS_CMD="-e robots=off"  # wget only, do not honor robots.txt rules.
+USERAGENT_FLAG=-U  # For curl, using '-A'
+USERAGENT_STRING="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36"
 
+########################## Congig END ##################################
 
-down_page()
-{
+function DetectEnv() {
+	which wget > /dev/null 2>&1
+	if [ $? -eq 0 ];then
+		CRAWLER=wget
+		echo -e "Using wget as crawler...\n"
+	else
+		which curl > /dev/null 2>&1
+		if [ $? -eq 0 ];then
+			CRAWLER=curl
+			OUTPUT_FLAG=-o
+			RETRY_FLAG=--retry
+			QUITE_FLAG=-s
+			FOLLOW_REDIRECT=-L
+			ROBOTS_CMD=
+			USERAGENT_FLAG=-A
+			echo -e "Using curl as crawler...\n"
+		else
+			echo -e "Neither wget nor curl was found, please check your system environment, aborting..."
+			exit 1
+		fi
+	fi
+}
+
+function DownPage() {
 	if [ -e $from ];then
 		rm -rf $from;
 	fi
 	mkdir $from;
 	local k=1;
 
-	if [ "$from" = "baidu" ];then			
+	if [ "$from" = "baidu" ];then
 		round=$(($num/60));
 		remain=$(($num - ${round}*60));
 
@@ -24,21 +61,28 @@ down_page()
 			touch $from/${k}_objURL-list_${query};
 			for((i=1;i<=$round;i++))
 			do
-				wget -t 3 -q "http://image.baidu.com/i?ct=201326592&cl=2&lm=-1&tn=baiduimagejson&istype=2&s=0&word=${query}&ie=utf-8&pn=$((($i-1)*60))&rn=60" -O $from/${k}_${query}_${i};
-
-				cat $from/${k}_${query}_${i}|grep objURL|awk -F'"' '{print $4}' >> $from/${k}_objURL-list_${query}
+				(
+				$CRAWLER $RETRY_FLAG $RETRY_NUM $USERAGENT_FLAG "$USERAGENT_STRING" "http://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&word=${query}&ie=utf-8&oe=utf-8&pn=$((($i-1)*60))&rn=60" $OUTPUT_FLAG $from/${k}_${query}_${i};
+				cat $from/${k}_${query}_${i}| egrep '"objURL":".*?"' -o | awk -F'"' '{print $4}' >> $from/${k}_objURL-list_${query}
+				) &
 			done
-
 			if [ $remain -ne 0 ];then
-				wget -t 3 -q "http://image.baidu.com/i?ct=201326592&cl=2&lm=-1&tn=baiduimagejson&istype=2&s=0&word=${query}&ie=utf-8&pn=$((($i-1)*60))&rn=${remain}" -O $from/${k}_${query}_${i};
-				cat $from/${k}_${query}_${i}|grep objURL|awk -F'"' '{print $4}'|sed 's/     //g' >> $from/${k}_objURL-list_${query}
+				(
+				$CRAWLER $RETRY_FLAG $RETRY_NUM $USERAGENT_FLAG "$USERAGENT_STRING" "http://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj?ct=201326592&word=${query}&ie=utf-8&oe=utf-8&pn=$((($i-1)*60))&rn=${remain}" $OUTPUT_FLAG $from/${k}_${query}_${i};
+				cat $from/${k}_${query}_${i}| egrep '"objURL":".*?"' -o | awk -F'"' '{print $4}' >> $from/${k}_objURL-list_${query}
+				) &
 			fi
-		
-			rm -rf $from/${k}_${query}*
+
+      # Waiting for background jobs to finish before removing temporary files.
+			wait
+			(
+			python decode_objurl.py $from/${k}_objURL-list_${query} $from/${k}_decoded_objURL-list_${query}
+			rm -rf $from/${k}_${query}* 
+			rm -rf $from/${k}_objURL-list_${query}
+			) &
 		
 			k=$(($k+1));
 		done < query_list.txt
-
 	elif [ "$from" = "google" ];then
 		round=$(($num/20));
 		remain=$(($num - ${round}*20));
@@ -48,22 +92,31 @@ down_page()
 			touch $from/${k}_objURL-list_${query};
 			for((i=1;i<=$round;i++))
 			do
-				wget -t 3 -q -e robots=off -U Mozilla "http://images.google.com/images?q=${query}&start=$((($i-1)*20))&sout=1" -O $from/${k}_${query}_${i};
-				cat $from/${k}_${query}_${i}|sed 's/href/\n/g'|sed 's/imgurl=/HSY/g'|sed 's/&amp;imgrefurl/HSY/g'|grep HSY|awk -F'HSY' '{print $2}' >> $from/${k}_objURL-list_${query};
+				(
+				$CRAWLER $RETRY_FLAG $RETRY_NUM $ROBOTS_CMD $FOLLOW_REDIRECT $USERAGENT_FLAG "$USERAGENT_STRING" "http://images.google.com/images?q=${query}&start=$((($i-1)*20))&sout=1" $OUTPUT_FLAG $from/${k}_${query}_${i};
+				# Non-greedy mode, to match only image url src part.
+				cat $from/${k}_${query}_${i}| egrep 'src="http.*?"' -o | awk -F'"' '{print $2}' >> $from/${k}_objURL-list_${query};
+				) &
 			done
 			
 			if [ $remain -ne 0 ];then
-				wget -t 3 -q -e robots=off -U Mozilla "http://images.google.com/images?q=${query}&start=$((($i-1)*20))&sout=1&num=${remain}" -O $from/${k}_${query}_${i};
-				cat $from/${k}_${query}_${i}|sed 's/href/\n/g'|sed 's/imgurl=/HSY/g'|sed 's/&amp;imgrefurl/HSY/g'|grep HSY|awk -F'HSY' '{print $2}' >> $from/${k}_objURL-list_${query};
+				(
+				$CRAWLER $RETRY_FLAG $RETRY_NUM $ROBOTS_CMD $FOLLOW_REDIRECT $USERAGENT_FLAG "$USERAGENT_STRING" "http://images.google.com/images?q=${query}&start=$((($i-1)*20))&sout=1&num=${remain}" $OUTPUT_FLAG $from/${k}_${query}_${i};
+			# Non-greedy mode, to match only image url src part.
+				cat $from/${k}_${query}_${i}| egrep 'src="http.*?"' -o | awk -F'"' '{print $2}' >> $from/${k}_objURL-list_${query}
+				) &
 			fi
 
-			rm -rf $from/${k}_${query}*
+			# Waiting for background jobs to finish before removing temporary files.
+			wait
+			rm -rf $from/${k}_${query}* &
 
 			k=$(($k+1));
 		done < query_list.txt
-
 	fi
 }
 
 ### main() ###
-down_page $num
+DetectEnv
+DownPage $num
+wait
